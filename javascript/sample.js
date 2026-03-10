ecbe6838708b3801c9463180ddd2f19268f07dcd
arya <aryava225@gmail.com>
2026-03-10 16:08:11 +0530

    success

---
+++ javascript/sample.js
@@ -0,0 +1,69 @@
+// let mark = 100;
+// if(mark>=90){
+//     console.log("A");
+
+// }
+// else if (mark>=80){
+//     console.log("B");
+// }
+// else if (mark>=70){
+//     console.log("C");
+// }
+
+// else{
+//     console.log("D");
+// }
+
+// // for (let i = 1;i <=10;i++){
+// //     console.log(i);
+// // }
+
+// let num =10;
+// if (num % 2 == 0){
+//     console.log("even number");
+// }
+// else{
+//     console.log("odd number");
+// }
+
+// let positive=1;
+// if(positive > 0){
+//     console.log("positive");
+
+// }
+// else{
+//     console.log("negative number");
+
+// }
+
+// let age = 22;
+// if (age >= 18){
+//     console.log("Eligible to vote");
+// }
+// else{
+//     console.log("not eligible to vote");
+// }
+// // for (let i = 10;i >= 1;i--){
+// //     console.log(i);
+// // }
+// // for (let i =1;i <=10;i++){
+// //     console.log(i * 5);
+// // }
+// for (let i=1;i<=20;i++){
+//     if(i % 2 == 0){
+//     console.log(i);
+
+// }
+// }
+
+// for (let i=1;i<=10;i++){
+//     if(i % 2 == 10){
+//     console.log(i);
+//     }
+// }
+
+// function addnumbers (num1,num2){
+//     let sum = num1 + num2;
+//     console.log(sum);
+// }
+// addnumbers(20,40);
