30b0517cc14792d9b170e51b1e8ed6598b1fa682
alfin jijo <alfinjijo2001@gmail.com>
2026-04-14 15:30:13 +0530

    succes

--- .NetCore/c#/Workshop/Workshop-4/Provider_App/Provider_App/obj/Debug/net8.0/Provider_App.AssemblyInfo.cs
+++ .NetCore/c#/Workshop/Workshop-4/Provider_App/Provider_App/obj/Debug/net8.0/Provider_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Provider_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151fcc5f5539783f39f2b097c50a02880d401f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8393c33ea4db48aa2d4089f7c1fc314217c1d316")]
 [assembly: System.Reflection.AssemblyProductAttribute("Provider_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Provider_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
