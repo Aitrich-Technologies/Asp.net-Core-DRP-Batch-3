f6e49a75e67b186b6c07bd3fb441a4ffa5ab95bd
shemeerarazak <shemeeraaitrich@gmail.com>
2026-05-13 11:04:05 +0530

    add

---
+++ .NetCore/Exception,Collection/Workshop/WorkShop (1)/WorkShop (1)/Models (1)/User (1).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Admin_Destination.Enums;
+
+namespace Admin_Destination.Models
+{
+    public  class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Roles Role { get; set; }
+
+        public User() { }
+
+        public User(int id, string name, string email, string password, Roles role)
+        {
+            Id = id;
+            FirstName = name;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+    }
+
+}
+
--- .NetCore/Oops/Workshop/workshop1/workshop1/obj/Debug/net8.0/workshop1.AssemblyInfo.cs
+++ .NetCore/Oops/Workshop/workshop1/workshop1/obj/Debug/net8.0/workshop1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("workshop1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c45f39c72b1ac224d3d572bb2a8df7c12756ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("workshop1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("workshop1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
