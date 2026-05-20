9137d5b8622d0d4692f0d8e7dc39756f4ce042ee
alfin jijo <alfinjijo2001@gmail.com>
2026-05-20 16:03:50 +0530

    Success

---
+++ .NetCore/c#/Alfin/Activity/activityarray1/activityarray1/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace activityarray1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //q1
+
+
+            //int[] numbers = { 30, 60, 90 };
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            //q2
+
+            //int[] numbers = { 30, 60, 90 };
+            //int sum = 0;
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+            //}
+            //Console.WriteLine("sum=" + sum);
+
+            //q3
+
+            //int[] numbers = { 30, 60, 90 };
+            //int max = numbers[0];
+
+            //for (int i = 1; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] > max)
+            //    {
+            //        max = numbers[i];
+            //    }
+            //}
+            //Console.WriteLine("Biggest number=" + max);
+
+            //q4
+
+            //int[] numbers = { 30, 60, 90 };
+
+            //Console.WriteLine("Total elements = " + numbers.Length);
+
+
+            //Question 5
+
+            int[] numbers = { 20, 40, 60 };
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ .NetCore/c#/Mariya/Enum/Enum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Enum
+{
+    enum Days
+    {
+        Sunday,     
+        Monday,     
+        Tuesday,    
+        Wednesday,  
+        Thursday,  
+        Friday,     
+        Saturday    
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Days today = Days.Wednesday;
+            Console.WriteLine("Day Name: " + today);
+
+
+        }
+
+    }
+}
+}
