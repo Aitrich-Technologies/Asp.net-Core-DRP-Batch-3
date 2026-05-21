dd14ebae86e11533101d2c48c4c82aff5f7dac59
alfin jijo <alfinjijo2001@gmail.com>
2026-05-21 15:32:27 +0530

    Success

---
+++ .NetCore/Exception,Collection/MachineTest/Sanjay/Exercise/Exercise/Enum/AccountType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise.Enum
+{
+    public enum AccountType
+    {
+        Savings = 1,
+        Current = 2
+    }
+}
