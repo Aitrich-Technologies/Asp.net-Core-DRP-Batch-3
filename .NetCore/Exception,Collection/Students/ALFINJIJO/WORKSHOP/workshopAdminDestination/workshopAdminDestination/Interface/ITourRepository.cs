1d9acb2dc507fe30a8a4473941886c138a219692
arya <aryava225@gmail.com>
2026-06-09 12:47:16 +0530

    add

---
+++ .NetCore/Exception,Collection/Students/ALFINJIJO/WORKSHOP/workshopAdminDestination/workshopAdminDestination/Interface/ITourRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using workshopAdminDestination.Models;
+
+namespace workshopAdminDestination.Interface
+{
+    public interface ITourRepository
+    {
+        void AddTour(Tour tour);
+        List<Tour> GetTours();
+    }
+
+}
---
+++ .NetCore/Exception,Collection/Students/ALFINJIJO/WORKSHOP/workshopAdminDestination/workshopAdminDestination/Manager/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using workshopAdminDestination.Interface;
+
+namespace workshopAdminDestination.Manager
+{
+    
+        public class CustomerManager : IMenu
+        {
+            public void DisplayMenu()
+            {
+                Console.WriteLine("1.View Tours\n2.Book Tour\n3.Logout");
+                Console.WriteLine("Booking feature can be extended...");
+            }
+        }
+
+    
+}
