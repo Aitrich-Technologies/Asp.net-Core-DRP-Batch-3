c41678afe4ff69d652789288d22683b1fbb7239b
shemeerarazak <shemeeraaitrich@gmail.com>
2026-06-10 11:58:01 +0530

    add

---
+++ .NetCore/Razor/Workshop/JobManagement/JobManagement/Extension/ApplicationServiceExtension.cs
@@ -0,0 +1,26 @@
+﻿using JobManagement.Helper;
+using JobManagement.Model;
+using JobManagement.Repository;
+using JobManagement.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobManagement.Extension
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices
+            (this IServiceCollection services,IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            // Add Services
+            services.AddScoped<JobService>();
+            services.AddScoped<JobRepository>();
+            // Add AutoMapper
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            return services;
+        }
+    }
+}
