3b9a05655036b496c781c7c17d480129cd1ac778
alfin jijo <alfinjijo2001@gmail.com>
2026-06-21 15:35:34 +0530

    add

--- .NetCore/Razor/Workshop/JobManagement/JobManagement/obj/Debug/net8.0/JobManagement.AssemblyInfo.cs
+++ .NetCore/Razor/Workshop/JobManagement/JobManagement/obj/Debug/net8.0/JobManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+151fcc5f5539783f39f2b097c50a02880d401f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945b4c710de781ec4b21ee18297ae0fc880aa34b")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
