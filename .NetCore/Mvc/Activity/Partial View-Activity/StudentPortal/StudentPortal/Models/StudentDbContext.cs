9f08241a255fb04543869b539e28574f0b074f99
shemeerarazak <shemeeraaitrich@gmail.com>
2026-07-03 11:14:15 +0530

    add

---
+++ .NetCore/Mvc/Activity/Partial View-Activity/StudentPortal/StudentPortal/Models/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Models
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) { }
+        
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=AFRA;Initial Catalog=DB_MVC;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+    }
+}
