582c28c1a79d471aaa6f9c74f01a7af3e7634d3c
shemeerarazak <shemeeraaitrich@gmail.com>
2026-07-17 14:58:06 +0530

    add

---
+++ .NetCore/WebApi/TDD/TDD/Manual testing/UnitTestSample/UnitTestSample/Program.cs
@@ -0,0 +1,10 @@
+﻿using UnitTestSample.Test;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Manual Testing Demo");
+        EvenOrOddNumberTest.EvenOrOddNumber_ReturnsEvenOrOddNumber_ReturnEvenNumber();
+        Console.ReadLine();
+    }
+}
---
+++ .NetCore/WebApi/TDD/TDD/Manual testing/UnitTestSample/UnitTestSample/Test/EvenOrOddNumberTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSample.Test
+{
+    public class EvenOrOddNumberTest
+    {
+        //Naming Convention - ClassName_MethodName_ExpectedResult
+        public static void EvenOrOddNumber_ReturnsEvenOrOddNumber_ReturnEvenNumber()
+        {
+            
+                //Arrange - Go get your variables, whatever you need your classes, get functions
+                int num = 8;
+          
+                EvenOrOddNumber evenOrOddNumber = new EvenOrOddNumber();
+
+                //Act - Executes the function
+                string Result = evenOrOddNumber.ReturnsEvenOrOddNumber(num);
+
+                //Assert - Whatever is returned is it what you want
+                if (Result == "Even Number")
+                {
+                    Console.WriteLine("Passed");
+                }
+                else
+                {
+                    Console.WriteLine("Fail");
+                }
+            }
+            
+        }
+    
+}
+
