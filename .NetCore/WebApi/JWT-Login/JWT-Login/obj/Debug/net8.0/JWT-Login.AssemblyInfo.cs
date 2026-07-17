4c1c40d44d8dddc755973d45deca8c3d33d40e68
shemeerarazak <shemeeraaitrich@gmail.com>
2026-07-17 11:16:12 +0530

    add

--- .NetCore/WebApi/JWT-Login/JWT-Login/obj/Debug/net8.0/JWT-Login.AssemblyInfo.cs
+++ .NetCore/WebApi/JWT-Login/JWT-Login/obj/Debug/net8.0/JWT-Login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWT-Login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfcd115be4c4f7fbd1f74209cf2d51278a6df3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWT-Login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWT-Login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
