b1c5c2c3f7f8380102b4104fefce4620e7225cfc
mariya baby <babymariya99@gmail.com>
2026-07-21 15:50:30 +0530

    add

--- .NetCore/WebApi/EmailOne/EmailOne/obj/Debug/net9.0/EmailOne.AssemblyInfo.cs
+++ .NetCore/WebApi/EmailOne/EmailOne/obj/Debug/net9.0/EmailOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmailOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c1c40d44d8dddc755973d45deca8c3d33d40e68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ecf1dadbf6a4201225e2d3a632a5be44db2cb21")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmailOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmailOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
